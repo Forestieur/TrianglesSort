4c5a88ddf96d70a576c79769b26cf69acdf5fad2
Forestieur <Forestieur@gmail.com>
2021-04-13 00:48:09 +0300

    Triangle and area

--- BL/Triangle.cs
+++ BL/Triangle.cs
@@ -8,5 +8,23 @@ namespace TrianglesSort.BL
 {
     class Triangle
     {
+        private string name { get; set; }
+        private double a { get; set; }
+        private double b { get; set; }
+        private double c { get; set; }
+
+        public Triangle(float A,float B, float C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+
+        public double Area()
+        {
+            double p = (a + b + c) / 2;
+
+            return Math.Sqrt(p * (p - a) *(p - b) * (p - c));
+        }
     }
 }
